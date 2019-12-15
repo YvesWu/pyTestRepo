10a14e7291559d251346809e65fb0bd11537cf76
Yves Wu <yves1014@gmail.com>
2019-12-15 20:51:03 +0800

    tryPassword

---
+++ tryPassword.py
@@ -0,0 +1,14 @@
+password = 'a123456'
+count = 3
+while True:
+	user_password = input('請輸入密碼:')
+	if (password == user_password):
+		print('登入成功')
+		break
+	else:
+		count -= 1
+		print('密碼錯誤')
+		if(count != 0):
+			print('你還有', count, '次機會' )
+	if(count == 0):
+		break
