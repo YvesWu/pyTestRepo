3b93c184327887171fa12093e78028f3318323fe
Yves Wu <yves1014@gmail.com>
2019-12-16 22:11:20 +0800

    add word length filter

--- reviews_analyzer.py
+++ reviews_analyzer.py
@@ -9,3 +9,9 @@ with open('reviews.txt', 'r') as f:
 reviews_length = len(reviews)
 averave_length = sum_length / reviews_length
 print('檔案讀取完了，總共有',len(reviews),'筆資料。其留言平均長度為', averave_length, '個字')
+
+sort_reviews = []
+for data in reviews:
+	if len(data) < 100:
+		sort_reviews.append(data)
+print('留言小於100字的共有', len(sort_reviews), '筆資料')
